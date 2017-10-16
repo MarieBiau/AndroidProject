fe337eaece5ab0c68836a82f5d184c9fe965867b
DBLJ <jean-alexandre.pecontal@edu.ece.fr>
2017-10-16 16:35:58 +0200

    added EventsView Back button

--- app/src/main/java/fr/android/app/androidproject/Events/EventsView.java
+++ app/src/main/java/fr/android/app/androidproject/Events/EventsView.java
@@ -1,17 +1,35 @@
 package fr.android.app.androidproject.Events;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import fr.android.app.androidproject.Main.MainActivity;
 import fr.android.app.androidproject.R;
 
 public class EventsView extends AppCompatActivity {
 
+    Button backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_view);
 
+        backButton = (Button) findViewById(R.id.backbutton);
+
+
+        backButton.setText("Back");
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(EventsView.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
 
     }
 }
