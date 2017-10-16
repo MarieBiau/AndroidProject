85bb58b39fe4b9bee7f6590e0f7dafcbc56e6592
MarieBiau <mbiaugui@enssat.fr>
2017-10-16 16:16:59 +0200

    textview

--- app/src/main/java/fr/android/app/androidproject/Main/MainActivity.java
+++ app/src/main/java/fr/android/app/androidproject/Main/MainActivity.java
@@ -22,21 +22,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
 
-        // rely txtview to variables
+        //rely textviews to variables
         textbloc1 = (TextView) findViewById(R.id.textView);
         textbloc2 = (TextView) findViewById(R.id.textView2);
         textbloc3 = (TextView) findViewById(R.id.textView3);
 
         //add text to txtview
-        textbloc1.setText("go to part A");
-        textbloc1.setText("go to part B");
-        textbloc1.setText("go to part C");
+        textbloc1.setText("go to part A (Maps)");
+        textbloc2.setText("go to part B (Events");
+        textbloc3.setText("go to part C (PostEvents)");
 
         /////////////////////////////////
         //click listener for textviews///
         /////////////////////////////////
+
         //listener txtview1
         textbloc1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EventsView.class);
+
+                startActivity(intent);
+            }
+        });
+
+        //listener txtview2
+        textbloc2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EventsView.class);
+
+                startActivity(intent);
+            }
+        });
+
+        //listener txtview3
+        textbloc3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, EventsView.class);
 
