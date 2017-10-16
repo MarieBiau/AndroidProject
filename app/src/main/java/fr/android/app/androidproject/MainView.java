c6922f0f5faea5914a34858cbc9cb6eb34826005
DBLJ <jean-alexandre.pecontal@edu.ece.fr>
2017-10-16 14:48:14 +0200

    changed activity name to view to respect architecture nom.

--- app/src/main/java/fr/android/app/androidproject/MainView.java
+++ app/src/main/java/fr/android/app/androidproject/MainView.java
@@ -3,7 +3,7 @@ package fr.android.app.androidproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
