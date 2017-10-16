1862a134ac335be093627eb2014bc18652e42714
DBLJ <jean-alexandre.pecontal@edu.ece.fr>
2017-10-16 16:52:03 +0200

    global structure of application : navigation bet. pages

--- app/src/main/java/fr/android/app/androidproject/PostEvents/PostEventsView.java
+++ app/src/main/java/fr/android/app/androidproject/PostEvents/PostEventsView.java
@@ -1,16 +1,36 @@
 package fr.android.app.androidproject.PostEvents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import fr.android.app.androidproject.Main.MainActivity;
 import fr.android.app.androidproject.R;
 
 public class PostEventsView extends AppCompatActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postevents_view);
+
+        backButton = (Button) findViewById(R.id.backbutton);
+
+
+        backButton.setText("Back");
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(PostEventsView.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 
 }
