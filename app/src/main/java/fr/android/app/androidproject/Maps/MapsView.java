0b34a040fafe51abecb97dcb82d6791c37274497
DBLJ <jean-alexandre.pecontal@edu.ece.fr>
2017-10-30 16:27:04 +0100

    merge from master and modified behavior of maps to ensure it display each time

--- app/src/main/java/fr/android/app/androidproject/Maps/MapsView.java
+++ app/src/main/java/fr/android/app/androidproject/Maps/MapsView.java
@@ -1,7 +1,11 @@
 package fr.android.app.androidproject.Maps;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,7 +27,11 @@ public class MapsView extends FragmentActivity implements OnMapReadyCallback {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (isNetworkAvailable()) {
+            mapFragment.getMapAsync(this);
+        }else{
+            Toast.makeText(this, "Error occured. Check your internet connection", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -44,5 +52,12 @@ public class MapsView extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
     }
 }
